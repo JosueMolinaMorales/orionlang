f7ff815ae3fab9b96df479a38c5a704c31190036
Josue Morales <molinajosue92@hotmail.com>
2023-09-03 13:07:55 -0400

    got built in function len working

--- internal/object/object.go
+++ internal/object/object.go
@@ -16,6 +16,7 @@ const (
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type (
@@ -24,7 +25,15 @@ type (
 		Type() ObjectType
 		Inspect() string
 	}
+	BuiltinFunction func(args ...Object) Object
 )
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
 
 type String struct {
 	Value string
