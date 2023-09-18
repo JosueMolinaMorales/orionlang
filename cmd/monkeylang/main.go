4e466251e1f722474a3da1689bd97a784476ab92
Josue Morales <molinajosue92@hotmail.com>
2023-09-18 17:38:04 -0400

    add flags to executable

--- cmd/monkeylang/main.go
+++ cmd/monkeylang/main.go
@@ -1,22 +1,58 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/user"
+	"strings"
 
+	"github.com/JosueMolinaMorales/monkeylang/internal/evaluator"
+	"github.com/JosueMolinaMorales/monkeylang/internal/lexer"
+	"github.com/JosueMolinaMorales/monkeylang/internal/object"
+	"github.com/JosueMolinaMorales/monkeylang/internal/parser"
 	"github.com/JosueMolinaMorales/monkeylang/internal/repl"
 )
 
+var (
+	filePath = flag.String("path", ".", "The file path to the file that should be interpreted")
+	runRepl  = flag.Bool("repl", false, "Run REPL for MonkeyLang")
+)
+
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
+	flag.Parse()
+
+	if *runRepl {
+		user, err := user.Current()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+
+		fmt.Printf("Feel free to type in commands\n")
+
+		repl.Start(os.Stdin, os.Stdout)
+		return
 	}
 
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	if !strings.HasSuffix(*filePath, ".mkl") {
+		log.Fatalf("File %s is not a MonkeyLang file", *filePath)
+	}
 
-	fmt.Printf("Feel free to type in commands\n")
+	fileInfo, err := os.ReadFile(*filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	repl.Start(os.Stdin, os.Stdout)
+	parser := parser.New(lexer.New(string(fileInfo)))
+	program := parser.ParseProgram()
+
+	if len(parser.Errors()) != 0 {
+		repl.PrintParserErrors(os.Stdout, parser.Errors())
+		return
+	}
+
+	evaluator.Eval(program, object.NewEnvironment())
 }
